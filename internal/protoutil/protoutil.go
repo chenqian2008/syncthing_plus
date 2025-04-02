96f24b4d14a70269cda372849b7fa2a584924112
Qian.Chen <1372692416@qq.com>
2025-04-02 13:22:40 +0800

    syncthing2.0beta.

---
+++ internal/protoutil/protoutil.go
@@ -0,0 +1,30 @@
+// Copyright (C) 2025 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package protoutil
+
+import (
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func MarshalTo(buf []byte, pb proto.Message) (int, error) {
+	if sz := proto.Size(pb); len(buf) < sz {
+		return 0, fmt.Errorf("buffer too small")
+	} else if sz == 0 {
+		return 0, nil
+	}
+	opts := proto.MarshalOptions{}
+	bs, err := opts.MarshalAppend(buf[:0], pb)
+	if err != nil {
+		return 0, err
+	}
+	if &buf[0] != &bs[0] {
+		panic("can't happen: slice was reallocated")
+	}
+	return len(bs), nil
+}
